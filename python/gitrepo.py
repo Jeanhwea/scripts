7fb18715d55c5e84f029a162e694ce36fe7b7a03
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-11-03 14:09:07 +0800

    [add] gitrepo.py

---
+++ python/gitrepo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import json
+import os
+import requests
+import sys
+
+class GitRepoCLI:
+
+
+  def __init__(self):
+    self.remote = 'github'
+    self.username = 'Jeanhwea'
+
+
+  def getRepos(self):
+    if self.remote == 'github':
+      url = 'https://api.github.com/users/{user}/repos'.format(
+        user = self.username
+      )
+      repos = requests.get(url).json()
+      for repo in repos:
+        print("{name}: {desc}".format(
+          name=repo["name"],
+          desc=repo["description"]
+        ))
+
+
+  def apply(self):
+    self.getRepos()
+
+
+
+def main():
+  cli = GitRepoCLI()
+  cli.apply()
+
+if __name__ == '__main__':
+  main()
